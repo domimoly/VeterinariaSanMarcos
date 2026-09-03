d8bdac8eb1e55f89f9b2a8590ab61b1962da1e58
molly <dom.ruiz@duocuc.cl>
2026-09-02 23:28:03 -0400

    Creación: Estructura Base del proyecto Se tomó la base del proyecto Conecta Cultura en cuanto al formato de página para aplicarlo según lo diseñado en el wireframe principalmente del index.

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const botonBienvenida = document.querySelector("#boton-bienvenida");
+
+if (botonBienvenida) {
+  botonBienvenida.addEventListener("click", () => {
+    alert("Veterinaria San Marcos se construye paso a paso.");
+  });
+}
