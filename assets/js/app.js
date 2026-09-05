5ae99f11cb3b4e3feb758ca1fb1699f83afae5e3
JoseeDx <jos.barrientosm@duocuc.cl>
2026-09-05 04:54:14 -0400

    Agrega servicios e iconos respectivos

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,15 @@ if (botonBienvenida) {
     alert("Veterinaria San Marcos se construye paso a paso.");
   });
 }
+
+// Lógica para abrir y cerrar el menú hamburguesa
+document.addEventListener('DOMContentLoaded', () => {
+    const btnMenu = document.getElementById('btn-menu');
+    const navegacion = document.getElementById('navegacion');
+
+    if(btnMenu && navegacion) {
+        btnMenu.addEventListener('click', () => {
+            navegacion.classList.toggle('activa');
+        });
+    }
+});
